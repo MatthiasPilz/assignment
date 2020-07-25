8d73373e5974aeca62b9db0a30aaa6462fff0881
MatthiasPilz <Matthias.Pilz@kingston.ac.uk>
2020-07-25 07:44:38 +0100

    task 2 - multiprocessing

---
+++ concurrency/test_multiprocessing.py
@@ -0,0 +1,42 @@
+import time
+import multiprocessing
+
+
+def num_integral_01(f, n, expected, myid):
+    x0 = 0.0
+    x1 = 1.0
+    del_x = (x1-x0) / n
+    result = 0.0
+    for i in range(1, n):
+        x = x0 + del_x*i
+        result += f(x)
+    result = 0.5*del_x*(f(x0) + f(x1) + 2.0*result)
+    print(myid, "--> error:", abs(result-expected))
+    return result
+
+
+def multiproc(n):
+    processes = []
+
+    for i in range(n):
+        p = multiprocessing.Process(target=num_integral_01, args=(lambda x: 3*x**2, 1000000, 1.0, i))
+        processes.append(p)
+        p.start()
+
+    for t in processes:
+        t.join()
+
+
+def main():
+    start = time.time()
+    multiproc(100)
+    end = time.time()
+    print("Took {:.5}s".format(end-start))
+
+
+if __name__ == "__main__":
+    main()
+    # 0 --> error: 4.785061236134425e-13
+    # ... (not in order)
+    # 99 --> error: 4.785061236134425e-13
+    # Took 1.8246s
