52be8dff89880e634c7a378145e5cbb11d5843e8
MatthiasPilz <Matthias.Pilz@kingston.ac.uk>
2020-07-25 06:59:41 +0100

    task 5 - base structure

---
+++ linkedList.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, content=None, next=None):
+        self.content = content
+        self.next = next
+
+
+class FibList:
+    def __init__(self, n: int=100):
+        if n == 0:
+            self.head = None
+        elif n == 1:
+            self.head = Node(content=0)
+        else:
+            self.head = Node(content=0, next=Node(content=1))
+            a = 0
+            b = 1
+            i = 2
+            cur = self.head.next
+            while i < n:
+                cur.next = Node(content=a+b)
+                a, b = b, a+b
+                i += 1
+                cur = cur.next
+
+    def println(self):
+        cur = self.head
+        output = str(cur.content)
+        while cur.next is not None:
+            cur = cur.next
+            output += " " + str(cur.content)
+        print(output)
+
+
+l1 = FibList()
+l1.println()
+l2 = FibList(10)
+l2.println()
